aaa3652f6701dc34ba714d46c50e7ecb3f19be44
Nelson Mestizo <nmestizo.bssltda@gmail.com>
2023-03-23 17:15:56 -0500

    conexion websocket

--- index.js
+++ index.js
@@ -1,34 +1,77 @@
-import WebSocket, { WebSocketServer } from 'ws';
+/*
+  Websocket server with express.js
+  (https://www.npmjs.com/package/express) and ws.js
+  (https://www.npmjs.com/package/ws)
+  Serves an index page from /public. That page makes
+  a websocket client back to this server.
 
-console.log("Server started");
-const Msg = '';
+  created 17 Jan 2021
+  modified 23 Feb 2023
+  by Tom Igoe
+*/
+// include express, http, and ws libraries:
+import express, { } from "express";
+// the const {} syntax is called destructuring.
+// it allows you to pull just the one function 
+// you need from the libraries below without 
+// making an instance of the whole library:
+import { createServer } from "http";
+import { WebSocketServer } from "ws";
+// make an instance of express:
+const app = express();
+// serve static content from the project's public folder:
+// app.use(app.static("public"));
+app.use(express.static('public'));
+// make an instance of http server using express instance:
+const server = createServer(app);
+// WebSocketServer needs the http server instance:
+const wss = new WebSocketServer({ server });
+// list of client connections:
+var clients = new Array();
 
-const wss = new WebSocketServer({
-  port: 3000,
-  perMessageDeflate: {
-    zlibDeflateOptions: {
-      // See zlib defaults.
-      chunkSize: 1024,
-      memLevel: 7,
-      level: 3
-    },
-    zlibInflateOptions: {
-      chunkSize: 10 * 1024
-    },
-    // Other options settable:
-    clientNoContextTakeover: true, // Defaults to negotiated value.
-    serverNoContextTakeover: true, // Defaults to negotiated value.
-    serverMaxWindowBits: 10, // Defaults to negotiated value.
-    // Below options specified as default values.
-    concurrencyLimit: 10, // Limits zlib concurrency for perf.
-    threshold: 1024 // Size (in bytes) below which messages
-    // should not be compressed if context takeover is disabled.
+// this runs after the http server successfully starts:
+function serverStart() {
+  var port = this.address().port;
+  console.log("Server listening on port " + port);
+}
+
+// this handles websocket connections:
+function handleClient(thisClient, request) {
+  // you have a new client
+  console.log("New Connection"); 
+  // add this client to the clients array
+
+  clients.push(thisClient); 
+  
+  function endClient() {
+    // when a client closes its connection
+    // get the client's position in the array
+    // and delete it from the array:
+    var position = clients.indexOf(thisClient);
+    clients.splice(position, 1);
+    console.log("connection closed");
   }
-});
 
-wss.on('connection', function(ws) {
-    ws.on('message', function(message) {
-    console.log('Received from client: %s', message);
-    ws.send('Server received from client: ' + message);
-});
-});
+  // if a client sends a message, print it out:
+  function clientResponse(data) {
+    console.log(data.toString());
+    broadcast(data.toString());
+  }
+
+  // This function broadcasts messages to all webSocket clients
+  function broadcast(data) {
+    // iterate over the array of clients & send data to each
+    for (let c in clients) {
+      clients[c].send(data);
+    }
+  }
+
+  // set up client event listeners:
+  thisClient.on("message", clientResponse);
+  thisClient.on("close", endClient);
+}
+
+// start the server:
+server.listen(process.env.PORT || 3000, serverStart);
+// start the websocket server listening for clients:
+wss.on("connection", handleClient);
